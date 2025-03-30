f15510999615175a75bb419d0f99d036fed109bb
jwenham <jrwenham22@gmail.com>
2025-03-30 12:56:56 -0700

    useEffect

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Demo = () => {
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        // The code we want to run
+        console.log("The count is:", count);
+
+        // Optional return function
+        return () => {
+            console.log('I am being cleaned up!');
+        };
+    }, [count]); // The dependency array 
+
     return (
         <div>
             <h1>Count: {count}</h1>
